72d9f71b5fcecb224575d0334e3b6a9f34319398
Junghyeon0329 <sptlsrja@gmail.com>
2024-11-13 15:16:58 +0900

    모델 수정

--- main.py
+++ main.py
@@ -24,29 +24,29 @@ print(tf.__version__)
 	0이면 가장 검은색 255면 가장 밝은 값    
 '''
 
-# (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-# ##  이미지 데이터를 255로 나누어 0과 1 사이로 변환하는 것은 신경망 모델이 효과적으로 학습할 수 있도록 돕는 중요한 전처리 단계입니다.
-# x_train, x_test = x_train / 255.0, x_test / 255.0
+##  이미지 데이터를 255로 나누어 0과 1 사이로 변환하는 것은 신경망 모델이 효과적으로 학습할 수 있도록 돕는 중요한 전처리 단계입니다.
+x_train, x_test = x_train / 255.0, x_test / 255.0
 # 데이터 자체의 프레임을 조절해야함.
 
-# model = tf.keras.models.Sequential([
-#     tf.keras.layers.Flatten(input_shape = (28,28)), ## 2D 이미지를 1D 벡터로 변환
-#     tf.keras.layers.Dense(239, activation='relu'), ## 239개의 노드를 가진 완전 연결(Dense) 레이어
-#     tf.keras.layers.Dropout(0.2), ## 20%의 노드를 무작위로 제거하여 과적합(overfitting)을 방지
-#     tf.keras.layers.Dense(32, activation='relu'),  ## 32개의 노드를 가진 완전 연결(Dense) 레이어 (RELU 활성화 함수)
-#     tf.keras.layers.Dropout(0.2), ## 또 다른 20%의 노드를 무작위로 제거
-#     tf.keras.layers.Dense(10) ## 최종 레이어는 10개의 노드를 가진 완전 연결(Dense) 레이어입니다. 이 레이어는 MNIST 데이터셋의 10개의 클래스를 예측하기 위한 출력 레이어입니다. 
-# ])
-# loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True)
-# model.compile(optimizer = 'adam', loss = loss_func, metrics = ['accuracy'])
-# model.fit(x_train, y_train, epochs = 10)
-# print(model.evaluate(x_test, y_test))
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape = (28,28)), ## 2D 이미지를 1D 벡터로 변환
+    tf.keras.layers.Dense(239, activation='relu'), ## 239개의 노드를 가진 완전 연결(Dense) 레이어
+    tf.keras.layers.Dropout(0.2), ## 20%의 노드를 무작위로 제거하여 과적합(overfitting)을 방지
+    tf.keras.layers.Dense(32, activation='relu'),  ## 32개의 노드를 가진 완전 연결(Dense) 레이어 (RELU 활성화 함수)
+    tf.keras.layers.Dropout(0.2), ## 또 다른 20%의 노드를 무작위로 제거
+    tf.keras.layers.Dense(10) ## 최종 레이어는 10개의 노드를 가진 완전 연결(Dense) 레이어입니다. 이 레이어는 MNIST 데이터셋의 10개의 클래스를 예측하기 위한 출력 레이어입니다. 
+])
+loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True)
+model.compile(optimizer = 'adam', loss = loss_func, metrics = ['accuracy'])
+model.fit(x_train, y_train, epochs = 10)
+print(model.evaluate(x_test, y_test))
 
-# a= np.array([[[1,2,3],[4,5,6]],[[0.1,0.2,0.3],[0.4,0.5,0.6]]])
-# b = np.array([[[10,20],[30,40],[50,60]],[[1,2],[3,4],[5,6]]])
-# print(np.matmul(a,b))
-# print(np.dot(a,b))
+a= np.array([[[1,2,3],[4,5,6]],[[0.1,0.2,0.3],[0.4,0.5,0.6]]])
+b = np.array([[[10,20],[30,40],[50,60]],[[1,2],[3,4],[5,6]]])
+print(np.matmul(a,b))
+print(np.dot(a,b))
 
 
 def logistic_regression(X, b, b0):
